bf19ba38fd656c8f5631dce5a70e77e5257dd7d2
su2708 <su2708@naver.com>
2021-10-18 18:31:02 +0900

    Create middleware

--- src/server.js
+++ src/server.js
@@ -4,16 +4,16 @@ const PORT = 4000;
 
 const app = express();
 
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+};
+
 const handleHome = (req, res) => {
-    return res.end();
+    return res.send("I love middlewares.");
 };
 
-const handleLogin = (req, res) => {
-    return res.send("Login here.");
-};
-
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.get("/", logger, handleHome);
 
 const handListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
 
