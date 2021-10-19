2aa6e5b72e5d8a81edbb40bf610e5d522cc24cb4
su2708 <su2708@naver.com>
2021-10-19 20:47:05 +0900

    Update routes

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,3 +1,6 @@
 export const homePage = (req, res) => res.send("Home Page Videos");
-export const watch = (req, res) => res.send("Watch Videos");
+export const search = (req, res) => res.send("Search");
+export const see = (req, res) => res.send("See Videos");
 export const edit = (req, res) => res.send("Edit Videos");
+export const remove = (req, res) => res.send("Remove Videos");
+export const upload = (req, res) => res.send("Upload Videos");
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { homePage } from "../controllers/videoController";
-import { join } from "../controllers/userController";
+import { homePage, search } from "../controllers/videoController";
+import { join, login } from "../controllers/userController";
 
 const globalRouter = express.Router();
 
 globalRouter.get("/", homePage);
 globalRouter.get("/join", join);
+globalRouter.get("/login", login);
+globalRouter.get("/search", search);
 
 export default globalRouter;
