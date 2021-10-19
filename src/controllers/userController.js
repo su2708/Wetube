e5de6f9c52d7a8f0f25108db88fff9dd252f8d2e
su2708 <su2708@naver.com>
2021-10-19 21:26:48 +0900

    Add id in routes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,4 +3,4 @@ export const login = (req, res) => res.send("Login");
 export const logout = (req, res) => res.send("Log out");
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
-export const see = (req, res) => res.send("See User");
+export const see = (req, res) => res.send(`See User #${req.params.id}`);
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,6 @@
 export const homePage = (req, res) => res.send("Home Page Videos");
 export const search = (req, res) => res.send("Search");
-export const see = (req, res) => res.send("See Videos");
-export const edit = (req, res) => res.send("Edit Videos");
-export const remove = (req, res) => res.send("Remove Videos");
+export const see = (req, res) => res.send(`See Videos #${req.params.id}`);
+export const edit = (req, res) => res.send(`Edit Videos #${req.params.id}`);
+export const remove = (req, res) => res.send(`Remove Videos #${req.params.id}`);
 export const upload = (req, res) => res.send("Upload Videos");
