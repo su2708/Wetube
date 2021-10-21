6d000f2a6bd02aab64ec00d0dcead893547ccfd7
su2708 <su2708@naver.com>
2021-10-21 18:35:41 +0900

    Extending Templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,6 @@
-export const homePage = (req, res) => res.render("home");
+export const homePage = (req, res) => res.render("home", {pageTitle: "Home"});
 export const search = (req, res) => res.send("Search");
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
+export const see = (req, res) => res.render("watch", {pageTitle: "Watch"});
+export const edit = (req, res) => res.render("edit", {pageTitle: "Edit"});
 export const remove = (req, res) => res.send(`Remove Videos #${req.params.id}`);
 export const upload = (req, res) => res.send("Upload Videos");
