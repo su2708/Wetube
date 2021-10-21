db53e866b7511fcf7e02ba50efd80d88c1b0197d
su2708 <su2708@naver.com>
2021-10-21 17:43:36 +0900

    Parting using pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,6 @@
-export const homePage = (req, res) => res.send("Home Page Videos");
+export const homePage = (req, res) => res.render("home");
 export const search = (req, res) => res.send("Search");
-export const see = (req, res) => res.send(`See Videos #${req.params.id}`);
-export const edit = (req, res) => res.send(`Edit Videos #${req.params.id}`);
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const remove = (req, res) => res.send(`Remove Videos #${req.params.id}`);
 export const upload = (req, res) => res.send("Upload Videos");
--- src/server.js
+++ src/server.js
@@ -11,6 +11,7 @@ const logger = morgan("dev");
 app.use(logger);
 
 app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
