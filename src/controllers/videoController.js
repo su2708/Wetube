897073dce9e48736e91efafa8c7f7ed32a813fd5
su2708 <su2708@naver.com>
2021-10-24 16:26:39 +0900

    Example of Iteration

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,7 @@
-export const homePage = (req, res) => res.render("home", {pageTitle: "Home"});
+export const homePage = (req, res) => {
+    const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    return res.render("home", {pageTitle: "Home", videos});
+};
 export const search = (req, res) => res.send("Search");
 export const see = (req, res) => res.render("watch", {pageTitle: "Watch"});
 export const edit = (req, res) => res.render("edit", {pageTitle: "Edit"});
