3c7e90bece3766c10b70e753c332151686551db9
su2708 <su2708@naver.com>
2021-11-18 00:19:21 +0900

    fake DB and how to edit

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,34 +1,49 @@
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes age",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes age",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes age",
+    views: 59,
+    id: 3,
+  },
+];
+
 export const homePage = (req, res) => {
-    const videos = [
-        {
-            title: "First Video",
-            rating: 5,
-            comments: 2,
-            createdAt: "2 minutes age",
-            views: 59,
-            id: 1,
-        },
-        {
-            title: "Second Video",
-            rating: 5,
-            comments: 2,
-            createdAt: "2 minutes age",
-            views: 59,
-            id: 1,
-        },
-        {
-            title: "Third Video",
-            rating: 5,
-            comments: 2,
-            createdAt: "2 minutes age",
-            views: 59,
-            id: 1,
-        },
-    ];
-    return res.render("home", {pageTitle: "Home", videos});
+  return res.render("home", { pageTitle: "Home", videos });
 };
-export const search = (req, res) => res.send("Search");
-export const see = (req, res) => res.render("watch", {pageTitle: "Watch"});
-export const edit = (req, res) => res.render("edit", {pageTitle: "Edit"});
-export const remove = (req, res) => res.send(`Remove Videos #${req.params.id}`);
-export const upload = (req, res) => res.send("Upload Videos");
+export const watch = (req, res) => {
+  const id = req.params.id;
+  const video = videos[id - 1];
+  return res.render("watch", {
+    pageTitle: `Watching ${video.title}`,
+    video: video,
+  });
+};
+export const getEdit = (req, res) => {
+  const id = req.params.id;
+  const video = videos[id - 1];
+  res.render("edit", { pageTitle: `Editing: ${video.title}`, video: video });
+};
+export const postEdit = (req, res) => {
+  const id = req.params.id;
+  const title = req.body.title;
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
+};
