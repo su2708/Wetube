e6447691dc6efcece1c57ea36f23d3b50ea97704
su2708 <su2708@naver.com>
2021-11-18 03:08:25 +0900

    Delete Search

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { homePage, search } from "../controllers/videoController";
+import homePage from "../controllers/videoController";
 import { join, login } from "../controllers/userController";
 
 const globalRouter = express.Router();
