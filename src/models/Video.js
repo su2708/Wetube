010a205d3ffd40564cd37dc89fd29e84584b55e6
su2708 <su2708@naver.com>
2021-11-20 01:34:13 +0900

    Creat models folder and Video.js

---
+++ src/models/Video.js
@@ -0,0 +1,17 @@
+import mongoose, { mongo } from "mongoose";
+import videoRouter from "../routers/videoRouter";
+
+const videoSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  createdAt: Date,
+  hashtags: [{ type: String }],
+  meta: {
+    views: Number,
+    rating: Number,
+  },
+});
+
+const Video = mongoose.model("Video", videoSchema);
+
+export default Video;
