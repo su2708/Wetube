f73f93695f5598bbdd29edd52491d97ae90a1e1e
su2708 <su2708@naver.com>
2021-11-23 17:27:31 +0900

    Creating a Video part

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -22,7 +22,17 @@ export const postEdit = (req, res) => {
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
-export const postUpload = (req, res) => {
-  const { title } = req.body;
+export const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+  await Video.create({
+    title: title,
+    descriptin: description,
+    createdAt: Date.now(),
+    hashtags: hashtags.split(", ").map((word) => `#${word}`),
+    meta: {
+      views: 0,
+      rating: 0,
+    },
+  });
   return res.redirect("/");
 };
