271d85171497e9a4bd68e55bae57d360736ea4f6
su2708 <su2708@naver.com>
2021-11-23 16:44:59 +0900

    Remove fake DB

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,64 +1,28 @@
-let videos = [
-  {
-    title: "First Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes age",
-    views: 59,
-    id: 1,
-  },
-  {
-    title: "Second Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes age",
-    views: 59,
-    id: 2,
-  },
-  {
-    title: "Third Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes age",
-    views: 59,
-    id: 3,
-  },
-];
+import Video from "../models/Video";
 
-export const homePage = (req, res) => {
+export const home = async (req, res) => {
+  const videos = await Video.find({});
   return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = (req, res) => {
-  const id = req.params.id;
-  const video = videos[id - 1];
+  const { id } = req.params;
   return res.render("watch", {
-    pageTitle: `Watching ${video.title}`,
-    video: video,
+    pageTitle: `Watching`,
   });
 };
 export const getEdit = (req, res) => {
-  const id = req.params.id;
-  const video = videos[id - 1];
-  res.render("edit", { pageTitle: `Editing: ${video.title}`, video: video });
+  const { id } = req.params;
+  res.render("edit", { pageTitle: `Editing` });
 };
 export const postEdit = (req, res) => {
-  const id = req.params.id;
-  const title = req.body.title;
-  videos[id - 1].title = title;
+  const { id } = req.params;
+  const { title } = req.body;
   return res.redirect(`/videos/${id}`);
 };
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
 export const postUpload = (req, res) => {
-  const newVideo = {
-    title: req.body.title,
-    rating: 0,
-    comments: 0,
-    createdAt: "just now",
-    views: 0,
-    id: videos.length + 1,
-  };
-  videos.push(newVideo);
+  const { title } = req.body;
   return res.redirect("/");
 };
