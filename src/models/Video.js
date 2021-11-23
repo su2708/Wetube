fe144b4005608c202bccf8412b572a8826de6b33
su2708 <su2708@naver.com>
2021-11-23 18:05:02 +0900

    More Schema

--- src/models/Video.js
+++ src/models/Video.js
@@ -2,10 +2,10 @@ import mongoose, { mongo } from "mongoose";
 import videoRouter from "../routers/videoRouter";
 
 const videoSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
+  title: { type: String, required: true, trim: true, maxlength: 80 },
+  description: { type: String, required: true, trim: true, minlength: 20 },
   createdAt: { type: Date, required: true, default: Date.now },
-  hashtags: [{ type: String }],
+  hashtags: [{ type: String, trim: true }],
   meta: {
     views: { type: Number, required: true, default: 0 },
     rating: { type: Number, required: true, default: 0 },
