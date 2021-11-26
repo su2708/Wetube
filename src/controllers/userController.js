b5946a541f13f4046f748cfed087f93bf1defe81
su2708 <su2708@naver.com>
2021-11-27 01:36:00 +0900

    Login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,8 @@
+// import bcrypt from "bcrypt";
 import User from "../models/User";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+
 export const postJoin = async (req, res) => {
   const { name, username, email, password, password2, location } = req.body;
   const pageTitle = "Join";
@@ -17,16 +19,47 @@ export const postJoin = async (req, res) => {
       errorMessage: "This username/email is already taken.",
     });
   }
-  await User.create({
-    name,
-    username,
-    email,
-    password,
-    location,
-  });
+  try {
+    await User.create({
+      name,
+      username,
+      email,
+      password,
+      location,
+    });
+  } catch (error) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: error._message,
+    });
+  }
   return res.redirect("/login");
 };
-export const login = (req, res) => res.send("Login");
+
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Log In" });
+
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const pageTitle = "Login";
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "An account with this username does not exists.",
+    });
+  }
+  // check if password correct
+  // const ok = await bcrypt.compare(password, user.password);
+  // if (!ok) {
+  //   return res.status(400).render("login", {
+  //     pageTitle,
+  //     errorMessage: "Wrong password.",
+  //   });
+  // }
+  return res.redirect("/");
+};
+
 export const logout = (req, res) => res.send("Log out");
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
