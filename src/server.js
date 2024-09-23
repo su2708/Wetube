e0ea63e03dc0aaf6f7b7a6249a931f3ced4eea90
su2708 <46125751+su2708@users.noreply.github.com>
2024-09-23 15:33:01 +0900

    GET Requests

--- src/server.js
+++ src/server.js
@@ -4,7 +4,9 @@ const PORT = 8000;
 
 const app = express();
 
+app.get("/", () => console.log("Somebody is trying to go home."))
+
 const handleListening = () =>
     console.log(`Server listening on http://localhost:${PORT}`);
 
-app.listen(PORT, handleListening)
+app.listen(PORT, handleListening);
