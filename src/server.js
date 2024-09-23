5c820dfa13e4fb0c6a2fc54500d4cd5744475771
su2708 <46125751+su2708@users.noreply.github.com>
2024-09-23 21:08:44 +0900

    Cleaning the Code

--- src/server.js
+++ src/server.js
@@ -1,23 +1,14 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 8000;
 
 const app = express();
 const logger = morgan("dev");
 app.use(logger);
-
-const globalRouter = express.Router();
-const userRouter = express.Router();
-const videoRouter = express.Router();
-
-const handleHome = (req, res) => res.send("Home");
-const handleEditUser = (req, res) => res.send("Edit User");
-const handleWatchVideo = (req, res) => res.send("Watch Video");
-
-globalRouter.get("/", handleHome);
-userRouter.get("/edit", handleEditUser);
-videoRouter.get("/watch", handleWatchVideo);
 
 app.use("/", globalRouter);
 app.use("/users", userRouter);
