94e97ac77ba1769bfc7e08160a6d2b87b358ac6b
su2708 <46125751+su2708@users.noreply.github.com>
2024-09-26 22:52:40 +0900

    Add variables to templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,6 @@
-export const trending = (req, res) => res.render("home");
-export const see = (req, res) => res.render("Watch");
-export const edit = (req, res) => res.render("Edit");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+export const see = (req, res) => res.render("Watch", { pageTitle: "Watch" });
+export const edit = (req, res) => res.render("Edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => res.send("Delete Video");
